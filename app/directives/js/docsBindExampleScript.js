02208ca320d3824f6a2442caf01ae67e20ceb831
alexanderwang <wangpei24@gmail.com>
2016-04-30 00:40:13 +0800

    Build the project and add a demo from AngularJS home page

---
+++ app/directives/js/docsBindExampleScript.js
@@ -0,0 +1,5 @@
+'use strict';
+var app = angular.module('docsBindExample', [])
+.controller('Controller', ['$scope', function($scope) {
+	$scope.name = 'Max Karl Ernst Ludwig Planck (April 23, 1858 – October 4, 1947)';
+}]);
